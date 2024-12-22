c4a0434a5205b5cb7aa56295d7cdfc94075b9de4
Pawan Gupta <erpawan459@gmail.com>
2024-12-22 18:07:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataValidation_RangeValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string number;
+            Console.WriteLine("Enter your age: ");
+            number = Console.ReadLine();
+
+            if ( int.TryParse(number,out int age) && age >= 18 && age <= 60)
+            {
+                Console.WriteLine("You are eligible to apply for the job.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to apply for the job.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
